4bb8799e8b1fab969cb78b2e8a486e798d6fc484
Akko <33564554+LvFinch@users.noreply.github.com>
2020-10-29 23:32:26 +0800

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-10-29 16:44:37
+ * @LastEditTime: 2020-10-29 17:25:19
+ * @LastEditors: Akko
+ * @Description: 
+ * @FilePath: /WebCalculator/myscript.js
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+/*背景光线部分*/
+let num = 40;//光线总数
+for(let i = 1; i < num; i++){
+    /*动态创建i标签*/
+    const i = document.createElement('i');
+    
+}
